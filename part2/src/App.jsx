6e8e37a94da5a6252bbb2033f887f60010aef623
Han Kim <han.dhk.kim@gmail.com>
2023-10-03 15:24:48 -0700

    Completed Exercise 2.5

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,32 +1,4 @@
-const Header = ({ name }) => <h2>{name}</h2>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    {parts.map(part => <Part key={part.id} part={part} />)}
-  </>
-
-const Total = ({ parts }) => {
-  const sum = parts.reduce((acc, curr) => acc + curr.exercises, 0)
-  
-  
-  return (<b>Number of exercises {sum}</b>)
-}
-
-
-const Course = ({ course }) => {
-  return(
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
