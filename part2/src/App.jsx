77fa8da56d5cc5432159e3e0291cc076484183bd
Han Kim <han.dhk.kim@gmail.com>
2023-10-03 01:31:44 -0700

    Completed Exercise 2.1

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,4 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
+const Header = ({ name }) => <h1>{name}</h1>
 
 const Total = ({ sum }) => <p>Number of exercises {sum}</p>
 
@@ -20,28 +20,39 @@ const Content = ({ parts }) =>
     />      
   </>
 
+const Course = ({ course }) => {
+  return(
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
+    </div>
+  )
+}
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <Course course={course}/>
     </div>
   )
 }
