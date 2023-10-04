488c2d9d179fa6cf619f9dffa845f823ef77ae22
Han Kim <han.dhk.kim@gmail.com>
2023-10-04 01:13:52 -0700

    Completed Exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,11 +2,22 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567'}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [search, setSearch] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  const personsToShow = search === '' 
+        ? persons 
+        : persons.filter(person => person.name.includes(search)) 
+
+  const searchHandler = (event) => {
+    setSearch(event.target.value)
+  }
 
   const buttonHandler = (event) => {
     event.preventDefault()
@@ -36,6 +47,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input
+        value={search} 
+        onChange={searchHandler}/>
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input 
@@ -52,7 +69,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      {personsToShow.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 }
