49852fd288efb6f2e263742068d4c908bd4fb32f
Han Kim <han.dhk.kim@gmail.com>
2023-10-04 00:05:09 -0700

    Completed Exercise 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,21 +4,37 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState('input name...')
+
+  const buttonHandler = (event) => {
+    event.preventDefault()
+
+    const toConcat = {
+      name: newName
+    }
+
+    setPersons(persons.concat(toConcat))
+  }
+
+  const changeHandler = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input 
+                 value={newName} 
+                 onChange={changeHandler} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={buttonHandler}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <div key={person.name}>{person.name}</div>)}
     </div>
   )
 }
